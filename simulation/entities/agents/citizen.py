64e1546b2df4738c28fecb7883f5e541e2fad942
SebastianJitaru <sjitaru29@gmail.com>
2024-09-08 12:35:09 +0200

    refactor: create new branch for citizen behaviour

--- simulation/entities/agents/citizen.py
+++ simulation/entities/agents/citizen.py
@@ -3,12 +3,16 @@ from .agent import Agent
 import numpy as np
 
 class Citizen(Agent):
+    """
+         A citizen agent in the environment.
+    """
     def __init__(self, position: np.ndarray, color: np.ndarray):
         super().__init__(position, color)
         self.role = "citizen"
         self.color = color
         
     def behaviour(self):
-        print("I am a citizen")
+        print("I am a citizen just don't steal from me")
+        
         
     
